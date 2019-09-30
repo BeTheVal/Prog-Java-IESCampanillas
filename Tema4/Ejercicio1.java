34ae56c1cfcffa5291fff1cbb2f881338b8863af
Miguel <miguelgutp@gmail.com>
2019-09-30 19:05:30 +0200

    Update exercise 1

--- Tema4/Ejercicio1.java
+++ Tema4/Ejercicio1.java
@@ -1,38 +1,38 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
-        System.out.print("Te sigo la asignatura que tienes a primera hora");
-        System.out.print("¿Que día es Hoy?");
-        respuesta = ( System.console().readLine().toLowerCase());
+        System.out.println("Te digo la asignatura que tienes a primera hora");
+        System.out.print("¿Que día es Hoy? : ");
+        String respuesta = System.console().readLine().toLowerCase();
         
-        if (respuesta.equal"lunes") {
-            System.out.print("Tienes Programación con Luis, mas te vale ser puntual");
+        if (respuesta.equals("lunes")) {
+            System.out.println("Tienes Programación con Luis, mas te vale ser puntual");
         } else {
             ;
         }
-        if (respuesta == "martes") {
-            System.out.print("Tienes Entornos de desarrollo con Pilar, mas te vale ser coger apuntes");
+        if (respuesta.equals("martes")) {
+            System.out.println("Tienes Entornos de desarrollo con Pilar, mas te vale ser coger apuntes");
 
         } else {
             ;
         }
-        if (respuesta == "miercoles") {
-        System.out.print("Tienes Programación con Luis, mas te vale ser puntual");
+        if (respuesta.equals("miercoles")) {
+        System.out.println("Tienes Programación con Luis, mas te vale ser puntual");
         } else {
             ;
         }
-        if (respuesta == "jueves") {
-            System.out.print("Tienes Programación con Luis, mas te vale ser puntual");
+        if (respuesta.equals("jueves")) {
+            System.out.println("Tienes Programación con Luis, mas te vale ser puntual");
         } else {
             ;
         }
-        if (respuesta == "viernes") {
+        if (respuesta.equals("viernes")) {
             System.out.print("Tienes Programación con Luis, mas te vale ser puntual");
         } else {
             ;
         }
 
-        if (respuesta == "sabado" || respuesta = "domigo") {
-            System.out.print("Tienes Programación con Luis, mas te vale ser puntual");
+        if (respuesta.equals("sabado") || respuesta.equals("domingo")) {
+            System.out.println("Creo que debes tomarte un café...");
         } else {
             ;
         }
