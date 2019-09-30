108c4eea797d6354a59a74c0e531133c107b521c
Miguel <miguelgutp@gmail.com>
2019-09-30 20:37:22 +0200

    Update exercises 2 and 3

--- Tema4/Ejercicio2.java
+++ Tema4/Ejercicio2.java
@@ -1,5 +1,22 @@
 public class Ejercicio2 {
     public static void main(String[] args) {
-        
+    System.out.print("Introduzca la Hora en formato AM (No introduzca los minutos) : ");
+    int hora = Integer.parseInt(System.console().readLine());
+    
+    if ((hora >= 6) && (hora <= 12)) {
+      System.out.println("Buenos días :)");
     }
+        
+    if ((hora >= 13) && (hora <= 20)) {
+      System.out.println("Buenas tardes :O");
+    }
+      
+    if (((hora >= 21) && (hora < 24)) || ((hora <= 5) && (hora >= 0))) {
+      System.out.println("Buenas noches -.-");
+    }
+      
+    if ((hora >= 24) || (hora < 0)) {
+      System.out.println("La hora introducida no es correcta.");
+    }
+  }
 }
--- Tema4/Ejercicio3.java
+++ Tema4/Ejercicio3.java
@@ -1,5 +1,28 @@
 public class Ejercicio3 {
     public static void main(String[] args) {
-        
+        System.out.print("Introduce un número del 1 al 7 y te diré a que día corresponde : ");
+        int numero = Integer.parseInt(System.console().readLine());
+
+        if (numero == 1){
+            System.out.println("Corresponde al lunes");
+        }
+        if (numero == 2){
+            System.out.println("Corresponde  al martes");
+        }
+        if (numero == 3){
+            System.out.println("Corresponde al miércoles");
+        }
+        if (numero == 4){
+            System.out.println("Corresponde el jueves");
+        }
+        if (numero == 5){
+            System.out.print("Corresponde el viernes");
+        }
+        if (numero == 6){
+            System.out.println("Corresponde el sábado");
+        }
+        if (numero == 7){
+            System.out.println("Corresponde el domingo");
+        }
     }
 }
