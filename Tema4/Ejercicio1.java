a0437d7138cdce227098c703f90227210f864417
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-09-30 18:22:07 +0200

    Update Ejercicio1

---
+++ Tema4/Ejercicio1.java
@@ -0,0 +1,42 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.print("Te sigo la asignatura que tienes a primera hora");
+        System.out.print("¿Que día es Hoy?");
+        respuesta = ( System.console().readLine().toLowerCase());
+        
+        if (respuesta.equal"lunes") {
+            System.out.print("Tienes Programación con Luis, mas te vale ser puntual");
+        } else {
+            ;
+        }
+        if (respuesta == "martes") {
+            System.out.print("Tienes Entornos de desarrollo con Pilar, mas te vale ser coger apuntes");
+
+        } else {
+            ;
+        }
+        if (respuesta == "miercoles") {
+        System.out.print("Tienes Programación con Luis, mas te vale ser puntual");
+        } else {
+            ;
+        }
+        if (respuesta == "jueves") {
+            System.out.print("Tienes Programación con Luis, mas te vale ser puntual");
+        } else {
+            ;
+        }
+        if (respuesta == "viernes") {
+            System.out.print("Tienes Programación con Luis, mas te vale ser puntual");
+        } else {
+            ;
+        }
+
+        if (respuesta == "sabado" || respuesta = "domigo") {
+            System.out.print("Tienes Programación con Luis, mas te vale ser puntual");
+        } else {
+            ;
+        }
+
+
+    }
+}
