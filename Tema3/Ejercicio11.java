ab7fa2abd38f1079ccb55be7727cea9fd519cff4
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-09-30 17:51:09 +0200

    Update Ejercicio11.java

--- Tema3/Ejercicio11.java
+++ Tema3/Ejercicio11.java
@@ -1,14 +1,13 @@
 public class Ejercicio11 {
     public static void main(String[] args) {
         
-        final double FactorDeConversion = 0.001;
+        final double factordeconsersion = 0.001;
         double totalmb, valorkb;
         
         System.out.print("Introduzca los Kb a convertir : ");
         valorkb = Double.parseDouble(System.console().readLine());
         
-        totalmb = (valorkb / FactorDeConversion);
-        System.out.println("Total en Mb : " + totalmb);
-        
+        totalmb = (valorkb / factordeconsersion);
+        System.out.println("Total en Mb : " + totalmb); 
     }
 }
