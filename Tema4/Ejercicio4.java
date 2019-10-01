ee168146ac77d563a119379f88e9e081333f1ec4
Miguel <miguelgutp@gmail.com>
2019-10-01 11:52:36 +0200

    Ejercicios propuestos finalizados

--- Tema4/Ejercicio4.java
+++ Tema4/Ejercicio4.java
@@ -1,5 +1,18 @@
+//calcula el salario semanal de un trabajador teniendo en cuenta 
+//que las horas ordinarias (40 primeras horas de
+//trabajo) se pagan a 12 euros la hora.
+// A partir de la hora 41, se pagan a 16 euros la hora.
 public class Ejercicio4 {
     public static void main(String[] args) {
         
+        System.out.print("Por favor, introduzca el número de horas trabajadas durante la semana: ");
+        int horastrabajadas = Integer.parseInt(System.console().readLine());
+        int sueldosemanal;
+        if (horastrabajadas < 40) {
+          sueldosemanal = 12 * horastrabajadas;
+        } else {
+          sueldosemanal = (40 * 12) + ((horastrabajadas - 40) * 16);
+        }
+        System.out.println("El sueldo semanal que le corresponde es de " + sueldosemanal + " euros");
     }
 }
--- Tema4/Ejercicio5.java
+++ Tema4/Ejercicio5.java
@@ -1,5 +1,16 @@
+//resuelve una ecuación de primer grado (del tipo ax+b = 0 ).
 public class Ejercicio5 {
     public static void main(String[] args) {
-        
+        System.out.print("Este programa resuelve ecuaciones de primer grado del tipo ax + b = 0");
+        System.out.print("Por favor, introduzca el valor de a");
+        //Asignación de variables
+        double a = Double.parseDouble( System.console().readLine() );
+        System.out.print("Por favor, introduzca el valor de b");
+        double b = Double.parseDouble( System.console().readLine() );
+        //Cálculo
+        double x = (-b/a);
+        System.out.println("X = " + x);
+
+
     }
 }
