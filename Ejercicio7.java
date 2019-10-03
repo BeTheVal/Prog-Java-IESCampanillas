6443972a60fa454f6414de8de03b00592a480009
Miguel <miguelgutp@gmail.com>
2019-10-03 09:02:23 +0200

    Update

--- Ejercicio7.java
+++ Ejercicio7.java
@@ -1,5 +1,13 @@
-public class Ejercicio7{
+public class Ejercicio7 {
     public static void main(String[] args) {
-        
+        System.out.println("Inserte la primera nota: ");
+        double primeraNota = Double.parseDouble( System.console().readLine());
+        System.out.println("Inserte la segunda nota: ");
+        double segundaNota = Double.parseDouble( System.console().readLine());
+        System.out.println("Inserte la tercera nota: ");
+        double terceraNota = Double.parseDouble( System.console().readLine());
+        //Media de la nota
+        double media = (primeraNota+segundaNota+terceraNota);
+        System.out.println("Tu media es de: " + media);
     }
 }
