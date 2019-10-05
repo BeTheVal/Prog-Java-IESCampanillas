11b82ace631782e6596b977fe6c6c158fcd20758
Miguel <miguelgutp@gmail.com>
2019-10-05 11:33:52 +0200

    Update

--- Tema4/Ejercicio11.java
+++ Tema4/Ejercicio11.java
@@ -1,2 +1,18 @@
 //dada una hora determinada (horas y minutos), calcule
 //los segundos que faltan para llegar a la medianoche.
+public class Ejercicio11 {
+    public static void main(String[] args) {
+        //Petición de datos
+        System.out.println("Esta aplicación calcula los segundos que faltan hasta la media noche a partir de la hora introducida");
+        System.out.println("Introduce la hora (sin los minutos):");
+        int hora = Integer.parseInt(System.console().readLine());
+        System.out.println("Introduce los minutos");
+        int minutos = Integer.parseInt(System.console().readLine());
+        final int segundosEnUnDia = 86400
+        //Conversiones
+        int horaEnSegundos = (hora * 60) * 60;
+        int minutosEnSegundos = (minutos * 60);
+
+
+    }
+}
