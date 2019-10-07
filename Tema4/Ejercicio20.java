562fdd09056bf37eb70c0b26489a7e1593b7d05c
Miguel <miguelgutp@gmail.com>
2019-10-07 12:36:43 +0200

    upload

--- Tema4/Ejercicio20.java
+++ Tema4/Ejercicio20.java
@@ -5,29 +5,24 @@ public class Ejercicio20 {
         
         System.out.print("Por favor, introduzca un número entero de hasta 5 : ");
         numero = Integer.parseInt(System.console().readLine());
-        // número de una cifra
         if (numero < 10) {
             esCapicua = true;
         }
-        // número de dos cifras
         if ((numero >= 10) && (numero < 100)) {
             if ((numero / 10) == (numero % 10)) {
             esCapicua = true;
             }
         }
-        // número de tres cifras
         if ((numero >= 100) && (numero < 1000)) {
             if ((numero / 100) == (numero % 10)) {
             esCapicua = true;
             }
         }
-        // número de cuatro cifras
         if ((numero >= 1000) && (numero < 10000)) {
             if (((numero / 1000) == (numero % 10)) && ((( numero / 100 ) % 10) == (( numero / 10) % 10))) {
             esCapicua = true;
             }
         }
-        // número de cinco cifras
         if (numero >= 10000) {
             if (((numero / 10000) == (numero % 10) ) && ((((numero / 1000) % 10)) == ((numero / 10) % 10))) {
             esCapicua = true;
