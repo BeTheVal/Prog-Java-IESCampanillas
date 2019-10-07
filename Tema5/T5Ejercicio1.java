b8c676735ad6d59ab0e0789a417aed78b9d5130c
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-10-07 18:36:39 +0200

    Doing exercises

---
+++ Tema5/T5Ejercicio1.java
@@ -0,0 +1,11 @@
+public class T5Ejercicio1 {
+    public static void main(String[] args) {
+        //Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.
+        for ( int i = 5 ; i < 100; i +=5 ) {
+            System.out.println(i);
+            if (i > 100){
+            break;
+            }
+        }
+    }
+}
---
+++ Tema5/T5Ejercicio2.java
@@ -0,0 +1,10 @@
+public class T5Ejercicio2{
+    public static void main(String[] args) {
+    // Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle while.
+        int num = 0;
+        while (num <= 100){
+            System.out.println(num);
+            num += 5;
+        }
+    }
+}
