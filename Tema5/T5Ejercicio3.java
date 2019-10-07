7f3d29946a3858ac5b7230345e7cbd2d9528b694
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-10-07 21:38:23 +0200

    Update

---
+++ Tema5/T5Ejercicio3.java
@@ -0,0 +1,10 @@
+public class T5Ejercicio3 {
+    public static void main(String[] args) {
+    //Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while.  
+    int num = 0;
+    do {
+        num += 5;
+        System.out.println(num);
+    } while (num < 100);      
+    }
+}
