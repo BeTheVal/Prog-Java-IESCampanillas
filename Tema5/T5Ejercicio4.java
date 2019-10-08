e09f89f93817ede670ae3d5d536d82b956a323a1
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-10-08 16:43:35 +0200

    Doing exercises

--- Tema5/T5Ejercicio4.java
+++ Tema5/T5Ejercicio4.java
@@ -1,10 +1,8 @@
 public class T5Ejercicio4 {
     public static void main(String[] args) {
     // Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando un bucle for .
-    int num;
-    for ( num = 320 ; num < 160 ; num -= 20 );
-        System.out.println(num);
-        num -= 20;
-
+        for ( int i = 320 ; i > 159; i -=20 ) {
+            System.out.println(i);
+        }
     }
 }
---
+++ Tema5/T5Ejercicio5.java
@@ -0,0 +1,11 @@
+public class T5Ejercicio5 {
+    public static void main(String[] args) {
+        //Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando un bucle while
+        int num = 320;
+        do {
+            System.out.println(num);
+            num -=20;
+
+        } while (num > 160);
+    }
+}
