12d5fe79d298997bc79da80a8a672f67acb5fea3
Miguel <miguelgutp@gmail.com>
2019-10-08 16:24:51 +0200

    Update

---
+++ Tema5/T5Ejercicio4.java
@@ -0,0 +1,10 @@
+public class T5Ejercicio4 {
+    public static void main(String[] args) {
+    // Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando un bucle for .
+    int num;
+    for ( num = 320 ; num < 160 ; num -= 20 );
+        System.out.println(num);
+        num -= 20;
+
+    }
+}
