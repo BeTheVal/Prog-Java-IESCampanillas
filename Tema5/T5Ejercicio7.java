3ac3950e2ca5646a579acc315d8bcac30690403e
Miguel <miguelgutp@gmail.com>
2019-10-09 12:18:31 +0200

    Creating exercises

---
+++ Tema5/T5Ejercicio7.java
@@ -0,0 +1,18 @@
+public class T5Ejercicio7 {
+    public static void main(String[] args) {
+        int intentosMaximos = 4;
+        int combinacion = 1234;
+        System.out.println("Bienvenido a la caja fuerte");
+        do {
+            System.out.println("Introduzca la clave para entrar(4 cifras):");
+            int clave = Integer.parseInt( System.console().readLine());
+            if (combinacion == clave) {
+                System.out.println("La clave introducida es correcta");
+                break;
+            } else {
+                System.out.println("La clave introducida es incorrecta");
+                intentosMaximos -= 1;
+            }
+        } while (intentosMaximos > 0);
+    }
+}
---
+++ Tema5/T5Ejercicio8.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class T5Ejercicio8{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Muestra la tabla de multiplicar de un número introducido por teclado.
+        int numero = Integer.parseInt(sc.nextLine());
+        System.out.println(numero + "*"+tabla + "="+ resultado);
+
+
+    }
+}
