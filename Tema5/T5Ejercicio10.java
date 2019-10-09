3bbd54e863980f14074d174636c6c09f4e4c074e
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-10-09 22:10:38 +0200

    Upload

---
+++ Tema5/T5Ejercicio10.java
@@ -0,0 +1,3 @@
+public class T5Ejercicio10 {
+    
+}
--- Tema5/T5Ejercicio9.java
+++ Tema5/T5Ejercicio9.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class T5Ejercicio9 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         //Realiza un programa que nos diga cuántos dígitos tiene un número introducido por teclado
+        int numeroDeDigitos = 1;
+        int n;
+        System.out.println("Introduzca un numero entero y te diré la cantidad de digitos");
+        int numeroIntroducido = Integer.parseInt(sc.nextLine());
+        n = numeroIntroducido;
+        while (n > 10){
+            n /=10;
+            numeroDeDigitos ++;
+        }
+        System.out.println(numeroIntroducido +" tiene "+ numeroDeDigitos +" digitos.");
     }
 }
