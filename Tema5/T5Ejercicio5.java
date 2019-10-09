4592fd43776b253c6aeb2e59090521502e40ce94
Miguel <miguelgutp@gmail.com>
2019-10-09 10:28:19 +0200

    Upload

---
+++ Tema5/T5Ejercicio5.java
@@ -0,0 +1,10 @@
+public class T5Ejercicio5 {
+    public static void main(String[] args) {
+        //Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando un bucle while .
+        int num = 320;
+        while (num > 159){
+            System.out.println(num);
+            num -= 20;
+        }
+    } 
+}
