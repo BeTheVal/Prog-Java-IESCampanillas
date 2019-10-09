0d051effbd792397c4355384245d1b3b383d0262
Miguel <miguelgutp@gmail.com>
2019-10-09 12:18:57 +0200

    More exercises

---
+++ Tema5/T5Ejercicio6.java
@@ -0,0 +1,9 @@
+public class T5Ejercicio6{
+    public static void main(String[] args) {
+        int num = 0;
+        do {
+            System.out.println(num);
+            num -= 20;
+        } while (num > 160);
+    }
+}
