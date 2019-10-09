90fc6961dd7c8b3beecf4b9c9a5b24f774cb840e
Miguel <miguelgutp@gmail.com>
2019-10-09 17:08:08 +0200

    To Do

--- Tema5/T5Ejercicio8.java
+++ Tema5/T5Ejercicio8.java
@@ -3,9 +3,12 @@ public class T5Ejercicio8{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //Muestra la tabla de multiplicar de un número introducido por teclado.
+        int tabla = 0;
         int numero = Integer.parseInt(sc.nextLine());
+        for(numero = 0;numero > 10;numero = + 1 )
+        resultado = numero*tabla;
         System.out.println(numero + "*"+tabla + "="+ resultado);
-
+        tabla += 1;
 
     }
 }
