41b455bdd7645d1623be1602c3d4c33c271910bd
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-10-10 21:10:17 +0200

    Update T5Ejercicio14.java

--- Tema5/T5Ejercicio14.java
+++ Tema5/T5Ejercicio14.java
@@ -1,29 +1,28 @@
 public class T5Ejercicio14 {
-
-    public static void main(String[] args) {
+  public static void main(String[] args) {
    
-        System.out.println("Cálculo de una potencia");
+    System.out.println("Cálculo de una potencia");
+
+    System.out.print("Introduzca la base: ");
+    int base = Integer.parseInt(System.console().readLine());
     
-        System.out.print("Introduzca la base: ");
-        int base = Integer.parseInt(System.console().readLine());
-        
-        System.out.print("Introduzca el exponente: ");
-        int exponente = Integer.parseInt(System.console().readLine());
-        double potencia = 1;   
-        if (exponente == 0) {
-          potencia = 1;
-        }
-        if (exponente > 0) {
-          for (int i = 0; i < exponente; i++) { //hasta que i sea mayor que el exponente
-            potencia *= base;
-          }
-        }
-        if (exponente < 0) {
-          for (int i = 0; i < -exponente; i++) {
-            potencia *= base;
-          } 
-          potencia = 1/potencia;
-        }    
-        System.out.println(base + "^" + exponente + " = " + potencia);
+    System.out.print("Introduzca el exponente: ");
+    int exponente = Integer.parseInt(System.console().readLine());
+    double potencia = 1;   
+    if (exponente == 0) {
+      potencia = 1;
+    }
+    if (exponente > 0) {
+      for (int i = 0; i < exponente; i++) { //hasta que i sea mayor que el exponente
+        potencia *= base;
+      }
+    }
+    if (exponente < 0) {
+      for (int i = 0; i < -exponente; i++) {
+        potencia *= base;
+      } 
+      potencia = 1/potencia;
     }    
-}
+    System.out.println(base + "^" + exponente + " = " + potencia);
+  }    
+  }
