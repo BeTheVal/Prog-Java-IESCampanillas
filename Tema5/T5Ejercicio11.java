e60869420613c3223045166cb98d78d98361c00c
Miguel <miguelgutp@gmail.com>
2019-10-10 10:16:23 +0200

    To do

---
+++ Tema5/T5Ejercicio11.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class T5Ejercicio11 {
+    public static void main(String[] args) {
+    //Escribe un programa que muestre en tres columnas, el cuadrado y el cubo de los 5 primeros números enteros a partir de uno que se introduce por teclado.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca número: ");
+        int numero = 0;
+
+    }
+}
