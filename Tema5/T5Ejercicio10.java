78745ad95470ee59d12ec18a9b57fc2aa1fdeba4
Miguel <miguelgutp@gmail.com>
2019-10-10 09:57:08 +0200

    fixing exercise 10

--- Tema5/T5Ejercicio10.java
+++ Tema5/T5Ejercicio10.java
@@ -3,15 +3,17 @@ public class T5Ejercicio10 {
     public static void main(String[] args) {
     // Escribe un programa que calcule la media de un conjunto de números positivos introducidos por teclado.
     Scanner sc = new Scanner(System.in);
-     int numero = 0;
+    int numero = 0;
     int contador = 0;
     int acumulador = 0;
      do{
          System.out.println("Introduzca un número entero: ");
          numero = Integer.parseInt(sc.nextLine());
          acumulador += numero;
-         contador += 1;
-     }while(numero > 0);
+         contador ++;
+     }while(numero >= 0);
+        acumulador -= numero;
+        contador -= 1;
         System.out.println("La media es: " + acumulador +" / "+ contador + " = " + acumulador/contador);
     }
 } 
