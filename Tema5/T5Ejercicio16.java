bce5d90af18bb6bee860148bc4dcb52c3b400deb
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-10-12 13:02:57 +0200

    Update

--- Tema5/T5Ejercicio16.java
+++ Tema5/T5Ejercicio16.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class T5Ejercicio16 {
     public static void main(String[] args) {
         // Escribe un programa que diga si un número introducido por teclado es o no primo.
+        
+
     }
 }
