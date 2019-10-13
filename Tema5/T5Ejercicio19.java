ef6ad80bd812452cbc1257ad8ed1c4ba8916a5bf
Miguel <miguelgutp@gmail.com>
2019-10-13 17:06:11 +0200

    Update

---
+++ Tema5/T5Ejercicio19.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class T5Ejercicio19 {
+    public static void main(String[] args) {
+        
+    
+    /*Realiza un programa que pinte una pirámide por pantalla. La altura se debe
+    pedir por teclado. El carácter con el que se pinta la pirámide también se debe
+    pedir por teclado.*/
+    Scanner sc = new Scanner(System.in);
+    System.out.print("¿Cual es la altura de la pirámide?: ");
+    int altura = Integer.parseInt(sc.nextLine());
+    System.out.print("¿Cual es el carácter de relleno de la pirámide?: ");
+    String caracterRelleno = sc.nextLine();
+
+    // para lineas
+    // para espacios
+
+
+
+
+
+
+    }
+
+}
