d28c3ee4dd8dc31de7d4b1c8511e6dea059508b8
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-10-15 17:18:20 +0200

    Update T5Ejercicio21.java

--- Tema5/T5Ejercicio21.java
+++ Tema5/T5Ejercicio21.java
@@ -7,11 +7,27 @@ public class T5Ejercicio21 {
         indicar el final de la introducción de datos pero no se incluye en el cómputo. */
         Scanner sc = new Scanner(System.in);
         int num = 0;
-        while (num < 0){
+        int aux = 0;
+        int almacenamiento = 0; 
+        int media= 0;
+        int mayorPares = 0;
+        while (num >= 0) {
             System.out.println("Inserte número :");
-            num = Integer.parseInt(sc.nextLine());
-
-            
+            num = Integer.parseInt(sc.nextLine());           
+            if (num %2 != 0){
+                for (int i = 0; i == 0; i++) {
+                    //Media de los impares
+                    almacenamiento += num; 
+                    aux ++;
+                    media = almacenamiento/aux;
+                }
+            }
+            if (num %2 == 0){
+                if (mayorPares < num){
+                    mayorPares = num;
+                }
+            }
         }
+        System.out.println("la media de los impares es: " + media + "y el mayor de los pares es: "+ mayorPares);
     }
 }
