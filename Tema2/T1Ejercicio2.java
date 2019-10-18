affa732bb7be99a0d18a15169e4578a366c12387
Miguel <miguelgutp@gmail.com>
2019-10-18 08:52:07 +0200

    Doing some exercises

---
+++ Tema2/T1Ejercicio2.java
@@ -0,0 +1,12 @@
+public class T1Ejercicio2 {
+  /*
+   * Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por
+   * pantalla de tal forma que el resultado del programa sea el mismo que en el
+   * ejercicio 1 del capítulo 1.
+   */
+  public static void main(String[] args) {
+    String nombre = "Miguel Gutiérrez";
+    System.out.println("Mi nombre es" + nombre);
+
+  }
+}
