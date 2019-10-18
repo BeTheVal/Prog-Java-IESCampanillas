6afe3543923fa993996ada388c24dc3e53845206
Miguel <miguelgutp@gmail.com>
2019-10-18 09:12:46 +0200

    T2Ejercicio3 Up

---
+++ Tema2/T2Ejercicio3.java
@@ -0,0 +1,16 @@
+public class T2Ejercicio3 {
+  public static void main(String[] args) {
+    /*
+     * Crea las variables nombre , direccion y telefono y asígnales los valores
+     * correspondientes. Muestra los valores de esas variables por pantalla de tal
+     * forma que el resultado del programa sea el mismo que en el ejercicio 2.
+     */
+    String nombre = "Miguel Gutiérrez";
+    String direccion = "Dirección: ";
+    String telefono = "Telefono";
+
+    System.out.println(nombre);
+    System.out.println(direccion);
+    System.out.println(telefono);
+  }
+}
