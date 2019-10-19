cd6fa7668b19f86b9bf17d6f802dffaa657deb05
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-10-19 16:35:26 +0200

    Doing Exercises

---
+++ Tema1/T1Ejercicio7.java
@@ -0,0 +1,10 @@
+public class T1Ejercicio7 {
+    /*Igual que el programa anterior, pero esta vez la pirámide estará hueca (se debe
+    ver únicamente el contorno hecho con asteriscos). */
+    public static void main(String[] args) {
+        System.out.printf(    "*"   );
+        System.out.printf(   "* *"   );
+        System.out.printf(  "*   *"   );
+        System.out.printf( "*******"   );
+    }
+}
