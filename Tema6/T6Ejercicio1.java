0d51d859cb17acc90635e1d74137b7bf4a05fa70
Miguel <miguelgutp@gmail.com>
2019-10-20 19:36:02 +0200

    Opening T6

---
+++ Tema6/T6Ejercicio1.java
@@ -0,0 +1,10 @@
+public class T6Ejercicio1 {
+  public static void main(String[] args) {
+    System.out.println("20 números aleatorios entre 1 y 10 (sin decimales):");
+    for (int i = 1; i <= 20; i++) {
+      System.out.print((int) (Math.random() * 10 + 1) + " ");
+      System.out.println();
+    }
+  }
+
+}
