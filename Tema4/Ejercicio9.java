dc9bc315260418a96f5ec42d5ed89b306bf5a324
Miguel <miguelgutp@gmail.com>
2019-10-21 09:28:11 +0200

    Restructuring

--- Tema4/Ejercicio9.java
+++ Tema4/Ejercicio9.java
@@ -3,23 +3,22 @@
 public class Ejercicio9 {
     public static void main(String[] args) {
         System.out.print("Ingrese a: ");
-        double a = Double.parseDouble( System.console().readLine());
+        double a = Double.parseDouble(System.console().readLine());
         System.out.print("Ingrese b: ");
-        double b = Double.parseDouble( System.console().readLine());
+        double b = Double.parseDouble(System.console().readLine());
         System.out.print("Ingrese c: ");
-        double c = Double.parseDouble( System.console().readLine());
-        if (a == 0 && b !=0 && c != 0) {
-        System.out.printlnf("La solución es %.2f", -c/b);
-        double auxiliar = (b*b) - (4* a * c);
-        if (auxiliar < 0){
-            System.out.println("Esta ecuación no tiene solución real");
-        } else {
-            double primerResultado = (-b + Math.sqrt(auxiliar)) / (2*a);
-            double segundoResultado = (-b - Math.sqrt(auxiliar)) / (2*a);
-            System.out.printf("Los resultados son: %.2f y %.2f\n" , primerResultado , segundoResultado);
+        double c = Double.parseDouble(System.console().readLine());
+        if (a == 0 && b != 0 && c != 0) {
+            System.out.println("La solución es %.2f", -c / b);
+            double auxiliar = (b * b) - (4 * a * c);
+            if (auxiliar < 0) {
+                System.out.println("Esta ecuación no tiene solución real");
+            } else {
+                double primerResultado = (-b + Math.sqrt(auxiliar)) / (2 * a);
+                double segundoResultado = (-b - Math.sqrt(auxiliar)) / (2 * a);
+                System.out.printf("Los resultados son: %.2f y %.2f\n", primerResultado, segundoResultado);
+            }
         }
-    }
 
-        
     }
 }
