24a9d140826b41b221427f2f23afe7d9d4e3c9a0
Miguel <miguelgutp@gmail.com>
2019-10-21 11:13:51 +0200

    Update

---
+++ Tema6/T6Ejercicio3.java
@@ -0,0 +1,8 @@
+public class T6Ejercicio3 {
+  public static void main(String[] args) {
+    /*
+     * Igual que el ejercicio anterior pero con la baraja española. Se utilizará la
+     * baraja de 40 cartas: 2, 3, 4, 5, 6, 7, sota, caballo, rey y as.
+     */
+  }
+}
---
+++ Tema6/T6Ejercicio5.java
@@ -0,0 +1,34 @@
+public class T6Ejercicio5 {
+  public static void main(String[] args) {
+    /*
+     * Muestra 50 números enteros aleatorios entre 100 y 199 (ambos incluidos)
+     * separados por espacios. Muestra también el máximo, el mínimo y la media de
+     * esos números.
+     */
+    System.out.println("20 números aleatorios entre 100 y 199:");
+    int media = 0;
+    int mayor = 0;
+    int menor = 120;
+    for (int i = 1; i <= 50; i++) {
+      // System.out.print(((int) (Math.random() * 50) + 100) + " ");
+      int numero = ((int) (Math.random() * 50) + 100);
+      System.out.println(numero);
+
+      // mayor
+      if (numero > mayor) {
+        mayor = numero;
+      }
+
+      // menor
+      if (numero < menor) {
+        menor = numero;
+      }
+
+      // Media
+      media += numero;
+    }
+    System.out.println("La media es : " + media / 50);
+    System.out.println("El menor es : " + menor + " y el mayor es : " + mayor);
+
+  }
+}
