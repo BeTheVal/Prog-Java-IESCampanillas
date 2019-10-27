ee5d8bbcd2a89b7ea4d83d02d8e41a651adf28aa
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-10-27 18:47:00 +0100

    Create T6Ejercicio10.java

---
+++ Tema6/T6Ejercicio10.java
@@ -0,0 +1,10 @@
+public class T6Ejercicio10 {
+	public static void main(String[] args) {
+		/*
+		 * Realiza un programa que pinte por pantalla diez líneas formadas por
+		 * caracteres. El carácter con el que se pinta cada línea se elige de forma
+		 * aleatoria entre uno de los siguientes: *, -, =, ., |, @. Las líneas deben
+		 * tener una longitud aleatoria entre 1 y 40 caracteres.
+		 */
+	}
+}
