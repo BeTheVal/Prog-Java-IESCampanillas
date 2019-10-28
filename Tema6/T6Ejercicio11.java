b58d6ab135dae9b4c0a317729979fe801151dc60
Miguel <miguelgutp@gmail.com>
2019-10-28 11:03:36 +0100

    Update T6E11

--- Tema6/T6Ejercicio11.java
+++ Tema6/T6Ejercicio11.java
@@ -6,5 +6,51 @@ public class T6Ejercicio11 {
      * final aparecerá el número de suspensos, el número de suficientes, el número
      * de bienes, etc.
      */
+    int numeroSuspenso = 0;
+    int numeroSuficiente = 0;
+    int numeroBien = 0;
+    int numeroNotable = 0;
+    int numeroSobresaliente = 0;
+
+    for (int i = 0; i < 20; i++) {
+      int notas = (int) ((Math.random() * 10) + 1);
+      if (notas < 4) {
+        System.out.println("suspenso");
+        numeroSuspenso++;
+      }
+      switch (notas) {
+      case 5:
+        System.out.println("Suficiente");
+        numeroSuficiente++;
+        break;
+      case 6:
+        System.out.println("Bien");
+        numeroBien++;
+        break;
+      case 7:
+        System.out.println("Notable");
+        numeroNotable++;
+        break;
+      case 8:
+        System.out.println("Notable");
+        numeroNotable++;
+        break;
+      case 9:
+        System.out.println("Sobresaliente");
+        numeroSobresaliente++;
+      case 10:
+        System.out.println("Sobresaliente");
+        numeroSobresaliente++;
+        break;
+      default:
+        break;
+      }
+    }
+    System.out.println("Cantidad sobresalientes : " + numeroSobresaliente);
+    System.out.println("Cantidad notables : " + numeroNotable);
+    System.out.println("Cantidad bien : " + numeroBien);
+    System.out.println("Cantidad suficientes : " + numeroSuficiente);
+    System.out.println("Cantidad suspensos : " + numeroSuspenso);
+
   }
 }
