e4891efe3aed86760353a5d0190ea6f675cd20e3
Miguel <miguelgutp@gmail.com>
2019-10-28 10:33:08 +0100

    Creating T6E11

---
+++ Tema6/T6Ejercicio11.java
@@ -0,0 +1,10 @@
+public class T6Ejercicio11 {
+  public static void main(String[] args) {
+    /*
+     * Escribe un programa que muestre 20 notas generadas al azar. Las notas deben
+     * aparecer de la forma: suspenso, suficiente, bien, notable o sobresaliente. Al
+     * final aparecerá el número de suspensos, el número de suficientes, el número
+     * de bienes, etc.
+     */
+  }
+}
