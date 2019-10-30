3dd30c092422a21beaf220e69818055a6d5d2fb5
Miguel <miguelgutp@gmail.com>
2019-10-30 08:58:46 +0100

    Update T6E14

--- Tema6/T6Ejercicio14.java
+++ Tema6/T6Ejercicio14.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class T6Ejercicio14 {
 	public static void main(String[] args) {
 		/*
@@ -7,6 +9,20 @@ public class T6Ejercicio14 {
 		 * programa debe preguntar si el número que estás pensando es mayor o menor que
 		 * el que te acaba de decir.
 		 */
+		Scanner sc = new Scanner(System.in);
+		int numeropensado = 0;
+		int contador = 0;
+		int adivinarNumero = 1;
+		int mayor = 100;
+		int menor = 0;
+		String mayorOMenor;
+		while (numeropensado != adivinarNumero || contador == 5) {
+			adivinarNumero = (int) ((Math.random() * 100) + 1);
+			System.out.print("¿Tu número es el " + adivinarNumero + " ?" );
+			System.out.print("¿Es mayor o menor?");
+			mayorOMenor = sc.nextLine()
+			contador++;
+		}
 
 	}
 }
