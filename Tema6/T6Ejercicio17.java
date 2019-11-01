00d27cc37edd1cbb305a6dee5cb497174f98ad9c
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-11-01 19:45:03 +0100

    Update T6Ejercicio17.java

--- Tema6/T6Ejercicio17.java
+++ Tema6/T6Ejercicio17.java
@@ -12,10 +12,36 @@ public class T6Ejercicio17 {
 		 * rectángulo.
 		 */
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Introduzca la altura de la pecera : ");
+		System.out.print("Introduzca la altura de la pecera (mínimo 4) : ");
 		int alturaPecera = sc.nextInt();
-		System.out.print("Introduzca la anchura de la pecera : ");
+		System.out.print("Introduzca la anchura de la pecera (mínimo 4) : ");
 		int anchuraPecera = sc.nextInt();
+
+		int posicion = 0;
+		int posicionPez = (int) (Math.random() * (alturaPecera - 2) * (anchuraPecera - 2));
+		/// Esto pinta la parte de arriba
+		for (int i = 0; i < anchuraPecera; i++) {
+			System.out.print("*");
+		}
+		System.out.println();
+		// Esto pinta la parte central
+		for (int i = 2; i < alturaPecera; i++) {
+			System.out.print("*"); // Parte izquierda de la pecera
+			for (int j = 2; j < anchuraPecera; j++) {
+				if (posicion == posicionPez) {
+					System.out.print("&");
+				} else {
+					System.out.print(" ");
+				}
+				posicion++;
+			} // for j
+			System.out.println("*"); // parte derecha de la pecera
+		} // for i
+
+		// Pinta la parte inferior /////////////////////////////////////////////////
+		for (int i = 0; i < anchuraPecera; i++) {
+			System.out.print("*");
+		}
 
 	}
 }
