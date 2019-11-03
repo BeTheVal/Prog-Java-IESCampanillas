e2608928d9a5f9006b6d409f775d79a548319aa8
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-11-03 21:11:42 +0100

    upload T6E19

--- Tema6/T6Ejercicio19.java
+++ Tema6/T6Ejercicio19.java
@@ -1,32 +1,30 @@
-import java.util.Scanner;
-
 public class T6Ejercicio19 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		/*
 		 * Escribe un programa que muestre 50 números enteros aleatorios comprendidos
 		 * entre el -100 y el 200 ambos incluidos y separados por espacios. Muestra
 		 * luego el máximo de los pares el mínimo de los impares y la media de todos los
 		 * números generados
 		 */
-		int contadorpositivos = 0;
-		int media = 0;
+		int maximoPares = 0;
+		int minimoImpares = 0;
+		int todosSumados = 0;
 		int aleatorio = 0;
 		for (int i = 0; i < 50; i++) {
 			aleatorio = (int) ((Math.random() * 300) - 101);
-			if (i == 49) {
-				System.out.print(aleatorio);
-			}
+			todosSumados += aleatorio;
 			System.out.print(aleatorio + ",");
 			if (aleatorio % 2 == 0) {
-				contadorpositivos++;
-				media += aleatorio;
-
+			} else if (aleatorio > maximoPares) {
+				maximoPares = aleatorio;
+			} else {
+				if (aleatorio < minimoImpares)
+					minimoImpares = aleatorio;
 			}
-
 		}
-
-		System.out.print("Media :" + media / contadorpositivos);
-
+		System.out.println();
+		System.out.println("Media de todos los números :" + todosSumados / 50);
+		System.out.println("Mayor de los pares: " + maximoPares);
+		System.out.println("Menor de los impares: " + minimoImpares);
 	}
 }
