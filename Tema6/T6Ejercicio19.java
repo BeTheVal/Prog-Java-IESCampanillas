6bcdf4188f8fd5ba509fd1d83a81195ed3069fd4
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-11-03 19:47:32 +0100

    Create T6E19

---
+++ Tema6/T6Ejercicio19.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class T6Ejercicio19 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		/*
+		 * Escribe un programa que muestre 50 números enteros aleatorios comprendidos
+		 * entre el -100 y el 200 ambos incluidos y separados por espacios. Muestra
+		 * luego el máximo de los pares el mínimo de los impares y la media de todos los
+		 * números generados
+		 */
+		int contadorpositivos = 0;
+		int media = 0;
+		int aleatorio = 0;
+		for (int i = 0; i < 50; i++) {
+			aleatorio = (int) ((Math.random() * 300) - 101);
+			System.out.print(aleatorio + ",");
+			if (aleatorio % 2 == 0) {
+				contadorpositivos++;
+				media += aleatorio;
+
+			}
+
+		}
+
+		System.out.print("Media :" + media / contadorpositivos);
+
+	}
+}
