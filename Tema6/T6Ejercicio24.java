a79e42b7af7d9fe9cc805e4e4a11d0e7209bf9d9
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-11-05 19:51:03 +0100

    T6E24 and 25 Update

---
+++ Tema6/T6Ejercicio24.java
@@ -0,0 +1,8 @@
+public class T6Ejercicio24 {
+	public static void main(String[] args) {
+		/*
+		 * Escribe un programa que, dado un número introducido por teclado, elija al
+		 * azar uno de sus dígitos.
+		 */
+	}
+}
