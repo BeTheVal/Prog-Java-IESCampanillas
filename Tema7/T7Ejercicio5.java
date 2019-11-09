73c72fa38904c18e115a511afc514ae35d9cbd59
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-11-09 13:30:13 +0100

    Create T7E5

---
+++ Tema7/T7Ejercicio5.java
@@ -0,0 +1,9 @@
+public class T7Ejercicio5 {
+	public static void main(String[] args) {
+		/*
+		 * Escribe un programa que pida 10 números por teclado y que luego muestre los
+		 * números introducidos junto con las palabras “máximo” y “mínimo” al lado del
+		 * máximo y del mínimo respectivamente.
+		 */
+	}
+}
