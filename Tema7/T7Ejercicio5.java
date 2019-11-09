f920d2be3fb696ae35e6137a1a3721ab58ce90f9
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-11-09 19:08:32 +0100

    T7E5

--- Tema7/T7Ejercicio5.java
+++ Tema7/T7Ejercicio5.java
@@ -1,9 +1,40 @@
+import java.util.Scanner;
+
 public class T7Ejercicio5 {
 	public static void main(String[] args) {
 		/*
 		 * Escribe un programa que pida 10 números por teclado y que luego muestre los
 		 * números introducidos junto con las palabras “máximo” y “mínimo” al lado del
 		 * máximo y del mínimo respectivamente.
+		 * 
 		 */
+		Scanner sc = new Scanner(System.in);
+		int maximo = Integer.MAX_VALUE;
+		int minimo = Integer.MIN_VALUE;
+		int numeros[] = new int[10];
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.println("Introduce número: ");
+			numeros[i] = sc.nextInt();
+			if (numeros[i] > minimo) {
+				minimo = numeros[i];
+			}
+
+			if (numeros[i] < maximo) {
+				maximo = numeros[i];
+			}
+
+		}
+		System.out.println();
+
+		for (int i = 0; i < numeros.length; i++) {
+			if (numeros[i] == maximo) {
+				System.out.println(numeros[i] + " - Mínimo ");
+			} else if (numeros[i] == minimo) {
+				System.out.println(numeros[i] + " - Máximo ");
+			}
+			System.out.println(numeros[i]);
+		}
+		sc.close();
+
 	}
 }
