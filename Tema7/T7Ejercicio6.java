ee64bbb1985d4689962980fb38b1fad909262760
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-11-10 12:29:56 +0100

    Upload T7E6

--- Tema7/T7Ejercicio6.java
+++ Tema7/T7Ejercicio6.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class T7Ejercicio6 {
 	public static void main(String[] args) {
 		/*
@@ -7,5 +9,26 @@ public class T7Ejercicio6 {
 		 * encuentra en la última posición debe pasar a la posición 0. Finalmente,
 		 * muestra el contenido del array.
 		 */
+		Scanner sc = new Scanner(System.in);
+		int i = 0;
+		int contador = 0;
+		int numerosfinal[] = new int[15];
+		int numeros[] = new int[15];
+		for (i = 0; i < numeros.length; i++) {
+			System.out.print("introduce un número: ");
+			numeros[i] = sc.nextInt();
+			System.out.println(numeros[i]);
+		}
+		for (int j = 0; j < 14; j++) {
+			numerosfinal[j + 1] = numeros[j];
+			contador++;
+		}
+		numerosfinal[0] = numeros[14];
+
+		System.out.println("-------------muestra la array--------------");
+		for (int j = 0; j < numerosfinal.length; j++) {
+			System.out.println(numerosfinal[j]);
+
+		}
 	}
 }
