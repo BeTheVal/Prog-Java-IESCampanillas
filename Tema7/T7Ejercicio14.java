b606f3365dcb81da44099d16e208775309afe216
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-11-14 09:25:26 +0100

    To Do

--- Tema7/T7Ejercicio14.java
+++ Tema7/T7Ejercicio14.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class T7Ejercicio14 {
 	public static void main(String[] args) {
 		/*
@@ -8,5 +9,32 @@ public class T7Ejercicio14 {
 		 * estar en otro array y son los siguientes: verde, rojo, azul, amarillo,
 		 * naranja, rosa, negro, blanco y morado.
 		 */
+		Scanner sc = new Scanner(System.in);
+		String[] palabras = new String[8];
+		String[] colores = {"verde", "rojo", "azul", "amarillo",
+			"naranja", "rosa", "negro", "blanco", "morado"};
+		String[] resultado = new String[8];
+		System.out.println("Introduce una palabra: ");
+		for (int i = 0; i < palabras.length; i++) {
+			palabras[i] = sc.nextLine();
+		}
+		int contador = 0;
+		int control = 7;
+		for(int x=0;x<colores.length;x++) {
+			for (int y = 0; y < palabras.length; y++) {
+			  if (colores[x].equals(palabras[y])){
+				System.out.println("El color "+colores[x]+" está repetido");
+				resultado[contador] = colores[x];
+				contador ++;
+			  } else{
+				  System.out.println("El color "+ colores[x]+" no está repetido");
+				  resultado[control] = colores[x];
+				  control --;
+
+
+
+			  }				
+			}
+		}
 	}
 }
