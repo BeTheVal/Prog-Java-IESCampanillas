ba330a4b78babb4b7f81fa7533bf6328480a061d
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-11-16 20:25:35 +0100

    Update T7E16

--- Tema7/T7Ejercicio16.java
+++ Tema7/T7Ejercicio16.java
@@ -11,12 +11,33 @@ public class T7Ejercicio16 {
 		 */
 		Scanner sc = new Scanner(System.in);
 		int[] numero = new int[20];
+		int contador = 0;
 		for (int i = 0; i < numero.length; i++) {
 			numero[i] = (int) (Math.random() * 400);
-			System.out.print(numero[i]);
-			System.out.println("1. Resaltar múltiplos de 5");
-			System.out.println("2. Resaltar múltiplos de 7");
-			int resaltar = sc.nextInt();
+			System.out.print(numero[i] + ", ");
+			if (contador == 19) {
+				System.out.println(numero[i] + ".");
+				System.out.println();
+			}
+			contador++;
+		}
+		System.out.println("1. Resaltar múltiplos de 5");
+		System.out.println("2. Resaltar múltiplos de 7");
+		int resaltar = sc.nextInt();
+		if (resaltar == 1) {
+			for (int i = 0; i < numero.length; i++) {
+				if (numero[i] % 5 == 0) {
+					System.out.print("[" + numero[i] + "]");
+				}
+			}
+		}
+		if (resaltar == 2) {
+			for (int i = 0; i < numero.length; i++) {
+				if (numero[i] % 7 == 0) {
+					System.out.println("[" + numero[i] + "]");
+
+				}
+			}
 		}
 
 	}
