eb5c44db0d36436718ef2d6c08d80c90b8e5f07e
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-11-18 12:51:29 +0100

    Create T7BEjercicio5.java

---
+++ Tema7/Bidimensionales/T7BEjercicio5.java
@@ -0,0 +1,13 @@
+public class T7BEjercicio5 {
+	public static void main(String[] args) {
+		/*
+		 * Realiza un programa que rellene un array de 6 filas por 10 columnas con
+		 * números enteros positivos comprendidos entre 0 y 1000 (ambos incluidos). A
+		 * continuación, el programa deberá dar la posición tanto del máximo como del
+		 * mínimo.
+		 */
+		// Generar la array con los numeros aleatorios, recorrer la array para buscar el
+		// maximo, recorrer la array para buscar la posicion del maximo.
+
+	}
+}
