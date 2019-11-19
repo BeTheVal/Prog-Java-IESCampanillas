a13d95637633ea3b5cad0e42eef7beb8f6558551
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-11-19 21:34:46 +0100

    Update T7BE6

--- Tema7/Bidimensionales/T7BEjercicio6.java
+++ Tema7/Bidimensionales/T7BEjercicio6.java
@@ -4,5 +4,47 @@ public class T7BEjercicio6 {
 		 * Modifica el programa anterior de tal forma que no se repita ningún número en
 		 * el array.
 		 */
+		int[][] num = new int[6][10];
+		int numeroMayor = 0;
+		int numeroMenor = 1001;
+		boolean esRepetido = false;
+		do {
+			for (int i = 0; i < num.length; i++) {
+				for (int j = 0; j < num[0].length; j++) {
+					num[i][j] = (int) (Math.random() * 1000);
+				}
+			}
+		} while (!esRepetido);
+
+		for (int i = 0; i < num.length; i++) {
+			for (int j = 0; j < num[0].length; j++) {
+				if (num[i][j] > numeroMayor) {
+					numeroMayor = num[i][j];
+				}
+			}
+		}
+
+		for (int i = 0; i < num.length; i++) {
+			for (int j = 0; j < num.length; j++) {
+				if (num[i][j] < numeroMenor) {
+					numeroMenor = num[i][j];
+				}
+			}
+		}
+		for (int i = 0; i < num.length; i++) {
+			for (int j = 0; j < num[0].length; j++) {
+				if (num[i][j] == numeroMayor) {
+
+				}
+			}
+		}
+		for (int i = 0; i < num.length; i++) {
+			for (int j = 0; j < num.length; j++) {
+				if (num[i][j] == numeroMenor) {
+					System.out.println("El número menor está en: " + i + "," + j);
+
+				}
+			}
+		}
 	}
 }
