12943afb980564a795288f4e5c53bd4116cfc011
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-11-19 20:33:07 +0100

    T7BE2 Refactorized

--- Tema7/Bidimensionales/T7BEjercicio2.java
+++ Tema7/Bidimensionales/T7BEjercicio2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class T7BEjercicio2 {
 	public static void main(String[] args) {
 		/*
@@ -6,6 +8,28 @@ public class T7BEjercicio2 {
 		 * sumas parciales de filas y columnas igual que si de una hoja de cálculo se
 		 * tratara. La suma total debe aparecer en la esquina inferior derecha.
 		 */
+		Scanner sc = new Scanner(System.in);
 		int[][] num = new int[4][5];
+		// Pedir los números e introducirlos en la array
+		for (int i = 0; i < num.length; i++) {
+			for (int j = 0; j < num[0].length; j++) {
+				System.out.print("Inserte número: ");
+				num[i][j] = sc.nextInt();
+				System.out.println();
+			}
+		}
+		// Mostrar la array
+		int suma = 0;
+		for (int i = 0; i < num.length; i++) {
+			for (int j = 0; j < num[0].length; j++) {
+				System.out.print(" " + num[i][j] + " ");
+				suma += num[i][j];
+				if (j == 4) {
+					System.out.print("Suma = " + suma);
+					suma = 0;
+				}
+			}
+			System.out.println();
+		}
 	}
 }
