b74ffd3d2a8dc3dabdae7def87df14d35fe41983
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-11-21 16:34:31 +0100

    Update T7BE12

--- Tema7/Bidimensionales/T7BEjercicio12.java
+++ Tema7/Bidimensionales/T7BEjercicio12.java
@@ -7,6 +7,41 @@ public class T7BEjercicio12 {
      * inferior izquierda a la esquina superior derecha, así como el máximo, el
      * mínimo y la media de los números que hay en esa diagonal.
      */
+    int[][] num = new int[9][9];
+    // asignar numeros aleatorios al array
+    for (int i = 0; i < num.length; i++) {
+      for (int j = 0; j < num[0].length; j++) {
+        num[i][j] = (int) ((Math.random() * 400) + 500);
+        System.out.print(" " + num[i][j] + " ");
+      }
+      System.out.println();
+    }
+    System.out.println();
+    // Recorrer la diagonal
+    int fila = 8;
+    int columna = 0;
+    int aux = 0;
+    int min = 901;
+    int maximo = 0;
+    int suma = 0;
+    for (int i = 0; i < num.length; i++) {
+      System.out.println(num[fila][columna]);
+      aux = num[fila][columna];
+      suma += aux;
+      if (aux > maximo) {
+        maximo = aux;
+      }
+      if (aux < min) {
+        min = aux;
+      }
+      columna++;
+      fila--;
+
+    }
+    System.out.println();
+    System.out.println("El máximo es: " + maximo);
+    System.out.println("El minimo es: " + min);
+    System.out.println("La media es: " + (suma / num.length));
 
   }
 }
