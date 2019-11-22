45ccda236f414c8f555644e9c18a3ed21aac0a88
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-11-22 21:32:09 +0100

    Update T8E1-14

--- Tema8/T8Ejercicio1al14.java
+++ Tema8/T8Ejercicio1al14.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class T8Ejercicio1al14 {
   public static void main(String[] args) {
     /*
@@ -27,5 +28,76 @@ public class T8Ejercicio1al14 {
      */
     Scanner sc = new Scanner(System.in);
     System.out.println("Elige lo que quieres hacer : ");
+    System.out.println(
+        "1.Es Capicua\n2.Es Primo\n3.Siguiente Primo\n4.potencia\n5.Cuenta digitos\n6.Voltea\n7.Digito en\n8.Posicion de digito\n9.Quita por detras\n10.Quita por delante\n11.Pega por detras\n12.Pega por delante\n13.Trozo de Numero\n14.Junta números");
+    System.out.print("Introduce número: ");
+    int eleccion = sc.nextInt();
+
+    switch (eleccion) {
+    case 1:
+
+      break;
+    case 2:
+
+      break;
+    case 3:
+
+      break;
+    case 4:
+
+      break;
+    case 5:
+
+      break;
+    case 6:
+
+      break;
+    case 7:
+
+      break;
+    case 8:
+
+      break;
+    case 9:
+
+      break;
+    case 10:
+
+      break;
+    case 11:
+
+      break;
+    case 12:
+
+      break;
+    case 13:
+
+      break;
+    case 14:
+
+    default:
+      break;
+    }
+
+  }
+
+  /////////////////////////////////////////////////// Funciones//////////////////////////////////////
+  /**
+   * Comprueba si un número es capicua o no.
+   */
+  public static boolean esCapicua(Long numero) {
+
+    long volteado = 0;
+
+    while (numero > 0) {
+      volteado = (volteado * 10) + (numero % 10);
+      numero /= 10;
+    } // while
+
+    if (volteado == numeroIntroducido) {
+      System.out.println("El " + numeroIntroducido + " es capicúa");
+    } else {
+      System.out.println("El " + numeroIntroducido + " no es capicúa");
+    }
   }
 }
