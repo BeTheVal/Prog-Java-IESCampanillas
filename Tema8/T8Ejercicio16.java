d9c04221db434ca4eac2340dcc00795cd2b79863
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2019-11-26 19:55:36 +0100

    Update T8E16 and 17

---
+++ Tema8/T8Ejercicio16.java
@@ -0,0 +1,14 @@
+import matematicas.Mates;
+
+public class T8Ejercicio16 {
+  public static void main(String[] args) {
+    /*
+     * Muestra los números capicúa que hay entre 1 y 99999.
+     */
+    for (int i = 1; i < 99999; i++) {
+      if (Mates.esCapicua(i)) {
+        System.out.println(i);
+      }
+    }
+  }
+}
---
+++ Tema8/T8Ejercicio17.java
@@ -0,0 +1,14 @@
+import matematicas.Mates;
+import java.util.Scanner;
+
+public class T8Ejercicio17 {
+  public static void main(String[] args) {
+    /*
+     * Escribe un programa que pase de binario a decimal.
+     */
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Introduce un numero en Binario: ");
+    long binario = sc.nextLong();
+
+  }
+}
