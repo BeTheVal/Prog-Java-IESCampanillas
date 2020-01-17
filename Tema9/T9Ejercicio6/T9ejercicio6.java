e19d37ce7fc913a15188e8264286319946dee465
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2020-01-17 13:32:51 +0100

    Upload T9E6

--- Tema9/T9Ejercicio6/T9ejercicio6.java
+++ Tema9/T9Ejercicio6/T9ejercicio6.java
@@ -3,7 +3,10 @@ package T9Ejercicio6;
 public class T9ejercicio6 {
     public static void main(String[] args) {
         Tiempo t1 = new Tiempo(1, 30, 25);
+        Tiempo t2 = new Tiempo(1, 30, 20);
+        t1.suma(t2);
         System.out.println(t1.toString());
-
+        t1.resta(t2);
+        System.out.println(t1.toString());
     }
 }
--- Tema9/T9Ejercicio6/Tiempo.java
+++ Tema9/T9Ejercicio6/Tiempo.java
@@ -13,14 +13,41 @@ public class Tiempo {
     }
 
     void suma(Tiempo TiempoASumar) {
-        if (this.segundos > 60) {
+        this.horas = this.horas + TiempoASumar.horas;
+        this.minutos = this.minutos + TiempoASumar.minutos;
+        this.segundos = this.segundos + TiempoASumar.segundos;
+        diferenciapositiva();
 
+    }
+    //TODO Terminar la resta
+
+    void resta(Tiempo TiempoARestar) {
+        this.horas = this.horas - TiempoARestar.horas;
+        this.minutos = this.minutos - TiempoARestar.minutos;
+        this.segundos = this.segundos - TiempoARestar.segundos;
+        if (this.horas < 0) {
+            int diferencia = this.horas - Math.abs(this.horas);
+            this.minutos = diferencia * 60;
         }
+        if (this.minutos < 0) {
+            int diferencia = this.minutos - Math.abs(this.minutos);
+            this.segundos = diferencia * 60;
+        }
+        diferenciapositiva();
 
     }
 
-    void resta(Tiempo TiempoARestar) {
-
+    private void diferenciapositiva() {
+        if (this.segundos >= 60) {
+            int diferencia = this.segundos - 60;
+            this.minutos++;
+            this.segundos = diferencia;
+        }
+        if (this.minutos >= 60) {
+            int diferencia = this.minutos - 60;
+            this.horas++;
+            this.minutos = diferencia;
+        }
     }
 
     @Override
