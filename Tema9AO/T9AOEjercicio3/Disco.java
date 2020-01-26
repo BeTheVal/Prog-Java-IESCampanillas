1a196c226f66cda4d7ea5c43ff82d029941b9885
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2020-01-26 12:42:17 +0100

    Upload T9AOE3

--- Tema9AO/T9AOEjercicio3/Disco.java
+++ Tema9AO/T9AOEjercicio3/Disco.java
@@ -1,4 +1,70 @@
 package T9AOEjercicio3;
 
 public class Disco {
+    private String codigo;
+    private String autor;
+    private String titulo;
+    private String genero;
+    private long duracion;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public long getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(long duracion) {
+        this.duracion = duracion;
+    }
+
+
+    public Disco(String codigo, String autor, String titulo, String genero, long duracion) {
+        this.codigo = codigo;
+        this.autor = autor;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracion = duracion;
+    }
+
+    public String toString() {
+        String cadena = "\n------------------------------------------";
+        cadena += "\nCódigo: " + this.codigo;
+        cadena += "\nAutor: " + this.autor;
+        cadena += "\nTítulo: " + this.titulo;
+        cadena += "\nGénero: " + this.genero;
+        cadena += "\nDuración: " + this.duracion;
+        cadena += "\n------------------------------------------";
+        return cadena;
+
+    }
 }
--- Tema9AO/T9AOEjercicio3/T9AOEjercicio3.java
+++ Tema9AO/T9AOEjercicio3/T9AOEjercicio3.java
@@ -7,26 +7,41 @@ public class T9AOEjercicio3 {
         Scanner sc = new Scanner(System.in);
         Disco[] gestor = new Disco[100];
         int contador = 0;
-
-        //Menú principal
-        System.out.println("Bienvenido a su gestor de discos.");
-        System.out.println("1. Añadir disco");
-        System.out.println("2.Mostrar discos almacenados");
-        System.out.println("3. Borrar disco");
-        System.out.println("4. salir");
-        System.out.println("Seleccione una opción: ");
-        int opcion = sc.nextInt();
+        int opcion = 0;
         do {
+            //Menú principal
+            System.out.println("Bienvenido a su gestor de discos.");
+            System.out.println("1. Añadir disco");
+            System.out.println("2.Mostrar discos almacenados");
+            System.out.println("3. Borrar disco");
+            System.out.println("4. salir");
+            System.out.println("Seleccione una opción: ");
+            opcion = sc.nextInt();
             switch (opcion) {
                 case 1:
-                    while (contador < 100) {
-                        System.out.println("");
+                    if (contador >= 100) {
+                        System.out.println("Actualmente tienes 100 discos, no se pueden almacenar más, debes borrar.");
+                        break;
                     }
-                    System.out.println("No su pueden almacenar más de 100 discos.");
+                    System.out.println("Introduzca el código del nuevo disco.");
+                    String codigo = sc.next();
+                    System.out.println("Introduzca el autor del nuevo disco.");
+                    String autor = sc.next();
+                    System.out.println("Introduzca el título del nuevo disco.");
+                    String titulo = sc.next();
+
+                    System.out.println("Introduzca el genero del nuevo disco.");
+                    String genero = sc.next();
+
+                    System.out.println("Introduzca la duración del nuevo disco.");
+                    long duracion = sc.nextLong();
+                    gestor[contador] = new Disco(codigo, autor, titulo, genero, duracion);
+                    System.out.println("Disco: " + gestor[contador].getTitulo() + " creado con exito.");
+                    contador++;
                     break;
                 case 2:
                     for (int i = 0; i < contador; i++) {
-                        Disco.toString();
+                        System.out.println(gestor[i].toString());
                     }
                     break;
                 case 3:
@@ -37,5 +52,4 @@ public class T9AOEjercicio3 {
             }
         } while (opcion != 4);
     }
-}
 }
