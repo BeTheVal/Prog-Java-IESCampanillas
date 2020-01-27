c8a3effad6d5f8e3872c6a87e973e371127bb120
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2020-01-27 11:13:39 +0100

    Upload T9AOEjercicio3

--- Tema9/T9Ejercicio3/Gato.java
+++ Tema9/T9Ejercicio3/Gato.java
@@ -1,10 +1,16 @@
 package T9Ejercicio3;
 
 public class Gato extends Mamifero {
-    public Gato(String nombre) {
-        super(nombre);
-    }
-    public void maullar(){
-        System.out.println("MIAAAUUU!");
-    }
+
+  public Gato(String nombre) {
+    super(nombre);
+  }
+
+  public Gato() {
+    super("nombre");
+  }
+
+  public void maullar() {
+    System.out.println("MIAAAUUU!");
+  }
 }
