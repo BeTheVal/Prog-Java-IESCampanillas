61801ebceb22f8c0cf1f5c0809cbc6912491d4ac
Miguel Valerio Gutiérrez Pardo <45542287+BeTheVal@users.noreply.github.com>
2020-01-29 09:15:51 +0100

    Refactor T10Ejercicio1

---
+++ Tema10/T10Ejercicio1.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class T10Ejercicio1 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    ArrayList<String> alumnos = new ArrayList<String>();
+    for (int i = 0; i < 6; i++) {
+      System.out.println("Introduce el nombre del alumno " + i);
+      alumnos.add(sc.next());
+    }
+    for (String alumno : alumnos) {
+      System.out.println(alumno);
+    }
+  }
+}
